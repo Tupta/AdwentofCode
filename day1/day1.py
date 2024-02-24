1b66a3898982abe2fb185cfda528da55145569c3
Tomasz Sendra <tomaszsendra@gmail.com>
2024-02-24 23:37:06 +0100

    second commit

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,21 @@
 ####day 1######
 
+# calibration document consists of lines of text; each line originally contained a specific calibration value. 
+# On each line, the calibration value can be found by combining the first digit and the last digit (in that order) 
+#to form a single two-digit number.
+
+# For example:
+
+# 1abc2
+# pqr3stu8vwx
+# a1b2c3d4e5f
+# treb7uchet
+
+# In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+# Consider your entire calibration document. What is the sum of all of the calibration values?
+
+#todo odczytanie z pliku tekstowego "day1" wszystkich liczb w kolejnych liniach i wpisanie ich jako kolejnych elementów słowniak
+#todo jeżeli jakaś liczba jest jednocyfrowa należy utworzyć z niej liczbę dwucyfrową poprzez jej zdublowanie
+#todo stowrzenie nowego słownika zawierającego liczby utworzone tylko z pierwszej i ostatniej liczby w poprzendim słowniku
+#todo zsumowanie wszystkich liczb dwucyfrowych w słowniku i podanie wyniku tej sumy.
