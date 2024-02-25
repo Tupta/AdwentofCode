3338a4c259fb908ad423ea29d04665720b5bbea1
Tomasz Sendra <tomaszsendra@gmail.com>
2024-02-25 21:50:32 +0100

    kolejny commit

--- day1/day1.py
+++ day1/day1.py
@@ -15,7 +15,42 @@
 
 # Consider your entire calibration document. What is the sum of all of the calibration values?
 
-#todo odczytanie z pliku tekstowego "day1" wszystkich liczb w kolejnych liniach i wpisanie ich jako kolejnych elementów słowniak
+#todo odczytanie z danych pliku tekstowego "day1" wszystkich liczb w kolejnych liniach i wpisanie ich jako kolejnych elementów słownika
 #todo jeżeli jakaś liczba jest jednocyfrowa należy utworzyć z niej liczbę dwucyfrową poprzez jej zdublowanie
 #todo stowrzenie nowego słownika zawierającego liczby utworzone tylko z pierwszej i ostatniej liczby w poprzendim słowniku
 #todo zsumowanie wszystkich liczb dwucyfrowych w słowniku i podanie wyniku tej sumy.
+
+data = open("C:\\Users\\Tupta\\Desktop\\AdwentofCode\\day1\\day1.txt", "r")
+#print(data.read())
+
+allData = []
+
+for line in data:
+    allData.append(line.strip())
+    
+#print(allData) 
+
+allNumbers = []
+for element in allData:
+   # print(element.strip())
+    for char in element:
+        if char.isdigit():
+            allNumbers.append(int(char))
+
+print(allNumbers)
+
+
+# data = open("C:\\Users\\Tupta\\Desktop\\AdwentofCode\\day1\\day1.txt", "r")
+
+# allData = []
+
+# for line in data:
+#     allData.append(line.strip())
+    
+# allNumbers = []
+# for element in allData:
+#     number_str = ''.join(char for char in element if char.isdigit())
+#     if number_str:
+#         allNumbers.append(int(number_str))
+
+# print(allNumbers)
