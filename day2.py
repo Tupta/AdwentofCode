f812a889afc8808dd10c683834412d42946d3579
Tomasz Sendra <tomaszsendra@gmail.com>
2024-03-14 15:03:23 +0100

    chyba final juz

--- day2.py
+++ day2.py
@@ -1,39 +1,18 @@
-# example_list = ["Game 1: 9 red, 5 blue, 6 green; 6 red, 13 blue; 2 blue, 7 green, 5 red",
-#                 "Game 2: 6 red, 2 green, 2 blue; 12 green, 11 red, 17 blue; 2 blue, 10 red, 11 green; 13 green, 17 red; 15 blue, 20 red, 3 green; 3 blue, 11 red, 1 green",
-#                 "Game 3: 20 green, 1 blue, 7 red; 20 green, 7 blue; 18 red, 8 green, 3 blue; 7 red, 6 blue, 11 green; 11 red, 6 blue, 16 green"]
 
-# # Inicjalizacja listy, która będzie przechowywać numery ID gier, które byłyby możliwe
-# possible_games = []
-
-# # Ograniczenia dotyczące liczby kostek czerwonych, zielonych i niebieskich
-# red_limit = 12
-# green_limit = 13
-# blue_limit = 14
-
-# for game in example_list:
-#     game_id, cubes_data = game.strip().split(': ')
-#     game_id = game_id.replace("Game ", "")
-#     print(game_id)
-#     cubes_sets = cubes_data.split(';')
-#     for cubes_set in cubes_sets:
-#         cubes = cubes_set.split(', ')
-#         print(cubes)
-        
-        
-        
-example_list = ["Game 1: 9 red, 5 blue, 6 green; 6 red, 13 blue; 2 blue, 7 green, 5 red",
-                "Game 2: 6 red, 2 green, 2 blue; 12 green, 11 red, 17 blue; 2 blue, 10 red, 11 green; 13 green, 17 red; 15 blue, 20 red, 3 green; 3 blue, 11 red, 1 green",
-                "Game 3: 20 green, 1 blue, 7 red; 20 green, 7 blue; 18 red, 8 green, 3 blue; 7 red, 6 blue, 11 green; 11 red, 6 blue, 16 green"]
+# Wczytanie danych z pliku tekstowego
+with open('day2.txt', 'r') as file:
+    games_data = file.readlines()
 
 # Inicjalizacja listy, która będzie przechowywać numery ID gier, które byłyby możliwe
 possible_games = []
+
 
 # Ograniczenia dotyczące liczby kostek czerwonych, zielonych i niebieskich
 red_limit = 12
 green_limit = 13
 blue_limit = 14
 
-for game in example_list:
+for game in games_data:
     game_id, cubes_data = game.strip().split(': ')
     game_id = game_id.replace("Game ", "")
     cubes_sets = cubes_data.split(';')
@@ -55,3 +34,5 @@ for game in example_list:
         possible_games.append(game_id)
 
 print("Possible games:", possible_games)
+
+print(sum(possible_games))
